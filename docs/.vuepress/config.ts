0f05a541d0d842429454c6b895581d9b617c8a95
221250108 <221250108@smail.nju.edu.cn>
2025-06-23 17:59:47 +0800

    新增评论区功能

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -165,17 +165,17 @@ export default defineUserConfig({
      * 评论 comments
      * @see https://theme-plume.vuejs.press/guide/features/comments/
      */
-    // comment: {
-    //   provider: '', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
-    //   comment: true,
-    //   repo: '',
-    //   repoId: '',
-    //   category: '',
-    //   categoryId: '',
-    //   mapping: 'pathname',
-    //   reactionsEnabled: true,
-    //   inputPosition: 'top',
-    // },
+    comment: {
+      provider: 'Giscus', // "Artalk" | "Giscus" | "Twikoo" | "Waline"
+      comment: true,
+      repo: 'ziqingchuan/ziqingchuan.github.io',
+      repoId: 'R_kgDOO_1C0A',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOO_1C0M4Cr4Nu',
+      mapping: 'pathname',
+      reactionsEnabled: true,
+      inputPosition: 'top',
+    },
 
     /**
      * 资源链接替换
