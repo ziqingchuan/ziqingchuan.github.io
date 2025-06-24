e321b2bcda3a92a4401083624ff100ec978a481e
221250108 <221250108@smail.nju.edu.cn>
2025-06-24 18:29:03 +0800

    2025-06-24

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -116,7 +116,7 @@ export default defineUserConfig({
      * markdown
      * @see https://theme-plume.vuejs.press/config/markdown/
      */
-    // markdown: {
+    markdown: {
     //   abbr: true,         // 启用 abbr 语法  *[label]: content
     //   annotation: true,   // 启用 annotation 语法  [+label]: content
     //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
@@ -143,7 +143,7 @@ export default defineUserConfig({
     //   },
     //   chartjs: true,      // 启用 chart.js
     //   echarts: true,      // 启用 ECharts
-    //   mermaid: true,      // 启用 mermaid
+      mermaid: true,      // 启用 mermaid
     //   flowchart: true,    // 启用 flowchart
     //   image: {
     //     figure: true,     // 启用 figure
@@ -153,7 +153,7 @@ export default defineUserConfig({
     //   },
       // include: true,      // 在 Markdown 文件中导入其他 markdown 文件内容
       // imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
-    // },
+    },
 
     /**
      * 水印
