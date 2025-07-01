7faa3889bc2d015680fec6265fbf6e42b36d7f26
221250108 <221250108@smail.nju.edu.cn>
2025-07-02 02:15:53 +0800

    2025-07-02

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -62,7 +62,7 @@ export default defineUserConfig({
       archives: true, // 是否启用归档页
       categories: true, // 是否启用分类页
       postCover: 'right', // 文章封面位置
-      pagination: 10, // 每页显示文章数量
+      pagination: 15, // 每页显示文章数量
     },
 
     /* 博客文章页面链接前缀 */
